96c3c92f06ada3a95514bd514299e188c59c0dfd
wcdoster <wcdoster@gmail.com>
2025-02-26 23:52:07 -0600

    add member count to board queries

--- src/controllers/BoardController.ts
+++ src/controllers/BoardController.ts
@@ -3,23 +3,30 @@ import { prisma } from "../index";
 
 const getAllBoards = async (req: Request, res: Response): Promise<void> => {
   try {
-    const boards = await prisma.board.findMany({
-      include: { createdBy: { select: { id: true, username: true } } },
-    });
-    res.status(200).json(boards);
+    const boards =
+      await prisma.$queryRaw`SELECT board.*, CAST(count(user.id) as VARCHAR) as memberCount FROM board LEFT JOIN boardsUsers ON board.id = boardsUsers.boardId LEFT JOIN user ON boardsUsers.memberID = user.id
+    `;
+
+    res.status(200).send(JSON.stringify(boards));
   } catch (e) {
+    console.log({ e });
     res.status(500).json({ error: e });
   }
 };
 
 const getBoardById = async (req: Request, res: Response): Promise<void> => {
   try {
-    const id = req.params.id;
+    const { id } = req.params;
     const board = await prisma.board.findUnique({
       where: { id },
-      include: { createdBy: { select: { id: true, username: true } } },
+      include: {
+        createdBy: { select: { id: true, username: true } },
+      },
     });
-    res.status(200).json(board);
+    const memberTotal = await prisma.board.findUnique({
+      where: { id },
+    });
+    res.status(200).json({ ...board, memberTotal: memberTotal });
   } catch (e) {
     res.status(500).json({ error: e });
   }
